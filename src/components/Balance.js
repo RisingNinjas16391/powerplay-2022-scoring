49cfe4b01eb23c57273f6417a02fa904b81b81d1
RisingNinjas16391 <57425816+RisingNinjas16391@users.noreply.github.com>
2022-07-13 08:44:34 -0400

    Updates

--- src/components/Balance.js
+++
@@ -1,9 +0,0 @@
-import { Grid } from "@mui/material";
-
-export default function Balance({ image }) {
-    return (
-        <Grid container justifyContent="center">
-            <img src={image} alt="Balance" />
-        </Grid>
-    );
-};
